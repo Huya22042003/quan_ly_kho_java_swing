74463846fc414fe318f05c2862d76a6026ea0eaa
thangbodoii <thangnvph26792@fpt.edu.vn>
2022-11-16 12:12:26 +0700

    no message

--- src/main/java/cores/truongPhongs/services/serviceImpls/NvqlQuanLyPhieuNhapServiceImpl.java
+++ src/main/java/cores/truongPhongs/services/serviceImpls/NvqlQuanLyPhieuNhapServiceImpl.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cores.truongPhongs.repositories;
+package cores.truongPhongs.services.serviceImpls;
 
 /**
  *
- * @author window
+ * @author Acer
  */
-public class TpQuanLyNvghRepository {
+public class NvqlQuanLyPhieuNhapServiceImpl {
     
 }
