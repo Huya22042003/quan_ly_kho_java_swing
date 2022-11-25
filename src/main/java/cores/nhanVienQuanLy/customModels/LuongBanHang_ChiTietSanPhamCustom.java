d08ecd311744142b54a73337b492ff6f40d7a3d0
taideptrai2003 <98102812+taideptrai2003@users.noreply.github.com>
2022-11-25 19:28:32 +0700

    update custom

---
+++ src/main/java/cores/nhanVienQuanLy/customModels/LuongBanHang_ChiTietSanPhamCustom.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cores.nhanVienQuanLy.customModels;
+
+import domainModels.DonVi;
+import domainModels.SanPham;
+import infrastructures.constant.MauConstant;
+import infrastructures.constant.TrangThaiSanPhamConstanst;
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LuongBanHang_ChiTietSanPhamCustom {
+    private UUID id;
+    private int soLuongTon;
+    private String hinhAnh;
+    private BigDecimal GiaNhap;
+    private BigDecimal GiaBan;
+    private int namBaoHanh;
+    private MauConstant mau;
+    private TrangThaiSanPhamConstanst trangThai;
+    private SanPham sanPham;
+    private DonVi donVi;
+}
